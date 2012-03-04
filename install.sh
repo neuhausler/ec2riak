684088548d29d4b7dbe08fee594dcb8ac5a363c8
Marcel Neuhausler <neuhausler@gmail.com>
2012-03-04 16:48:21 -0500

    Added riak.initd

--- install.sh
+++ install.sh
@@ -43,9 +43,8 @@ adduser --system --home /usr/local/var/lib/riak -M --shell /bin/bash --comment "
 chown -R riak:riak /usr/local/etc/riak /usr/local/var/lib/riak /usr/local/var/log/riak /usr/local/lib/riak
 
 # put changed init.d script in place
-# cp $INSTALL_DIR/riak /usr/local/lib/riak/bin
-chmod 0755 /usr/local/lib/riak/bin/riak
-cp /usr/local/lib/riak/bin/riak /usr/local/etc/rc.d/
+cp $INSTALL_DIR/riak.initd /usr/local/etc/rc.d/
+chmod 0755 /usr/local/etc/rc.d/riak
 ln -s /usr/local/etc/rc.d/riak /etc/init.d/riak
 
 
